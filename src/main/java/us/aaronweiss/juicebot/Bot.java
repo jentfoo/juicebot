c93c1bdea4b0cb1a19bd3d7fe460d1ed62520429
Aaron Weiss <aaronweiss74@gmail.com>
2013-03-10 23:21:22 -0400

    Added gitignore, converted to maven, upgraded for Netty 4.

---
+++ src/main/java/us/aaronweiss/juicebot/Bot.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2012 Aaron Weiss <aaronweiss74@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining 
+ * a copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom 
+ * the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included 
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package us.aaronweiss.juicebot;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.BufType;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.oio.OioEventLoopGroup;
+import io.netty.channel.socket.oio.OioSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
+
+import java.net.InetSocketAddress;
+
+import us.aaronweiss.juicebot.net.BotHandler;
+
+/**
+ * The abstract base for all bots, as it handles network bootstrapping.
+ * @author Aaron Weiss
+ * @version 1.0
+ * @since 1.0
+ */
+public abstract class Bot implements IBot {
+	protected final Bootstrap bootstrap;
+	protected final Configuration config;
+	protected Channel session;
+	
+	public Bot(String username, String server, String port) {
+		bootstrap = new Bootstrap();
+		bootstrap.group(new OioEventLoopGroup());
+		bootstrap.channel(OioSocketChannel.class);
+		bootstrap.handler(new ChannelInitializer<OioSocketChannel>() {
+			@Override
+			protected void initChannel(OioSocketChannel ch) throws Exception {
+				ChannelPipeline pipeline = ch.pipeline();
+				// Decoders
+				 pipeline.addLast("frameDecoder", new LineBasedFrameDecoder(1000));
+				 pipeline.addLast("stringDecoder", new StringDecoder(CharsetUtil.UTF_8));
+				 pipeline.addLast("botHandler", new BotHandler(Bot.this));
+
+				 // Encoder
+				 pipeline.addLast("stringEncoder", new StringEncoder(BufType.BYTE, CharsetUtil.UTF_8));
+			}
+		});
+		bootstrap.option(ChannelOption.TCP_NODELAY, true);
+		bootstrap.option(ChannelOption.SO_KEEPALIVE, true);
+		config = new Configuration(username, server, port);
+		config.put("BOT_PERIODIC", "-1");
+	}
+	
+	public abstract void onConnect();
+
+	public void onDisconnect() {
+		this.session = null;
+	}
+
+	public void periodic() {
+		BotUtils.output("BOT_PERIODIC is set without overriding default Bot.periodic()");
+	}
+	
+	public void connect() {
+		BotUtils.output("Connecting to " + config.get("IRC_SERVER") + ":" + config.get("IRC_PORT"));
+		ChannelFuture cf = this.bootstrap.connect(new InetSocketAddress(config.get("IRC_SERVER"), Integer.parseInt(config.get("IRC_PORT"))));
+		cf.syncUninterruptibly();
+		this.session = cf.channel();
+	}
+	
+	public void write(String message) {
+		this.session.write(message);
+	}
+	
+	public void disconnect() {
+		ChannelFuture cf = this.session.disconnect();
+		cf.syncUninterruptibly();
+		this.session = null;
+	}
+
+	public boolean isConnected() {
+		return this.session.isActive();
+	}
+	
+	public Configuration getConfiguration() {
+		return config;
+	}
+
+}
---
+++ src/main/java/us/aaronweiss/juicebot/examples/JuiceBot.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2012 Aaron Weiss <aaronweiss74@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining 
+ * a copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom 
+ * the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included 
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package us.aaronweiss.juicebot.examples;
+
+import java.util.Scanner;
+
+import us.aaronweiss.juicebot.AutoBot;
+import us.aaronweiss.juicebot.BotBootstrap;
+import us.aaronweiss.juicebot.BotUtils;
+
+/**
+ * A basic IRC bot, who just wants some juice. 
+ * @author Aaron Weiss
+ * @version 1.0
+ * @since 1.0
+ */
+public class JuiceBot extends AutoBot {
+	public JuiceBot(String server, String port) {
+		super("JuiceBot", server, port);
+	}
+
+	@Override
+	public void onConnect() {
+		BotUtils.register(this);
+		BotUtils.join("#vana", this);
+	}
+	
+	@Override
+	public void onMessage(String[] message) {
+		boolean isAdminCommand = false;
+		if (message[0].startsWith(":" + config.get("BOT_OWNER") + "!")) {
+			if (message[1].equals("PRIVMSG")) {
+				String line = BotUtils.joinStringFrom(message, 3);
+				if (line.startsWith(":JuiceBot:") && (line.contains("quit") || line.contains("gtfo"))) {
+					BotUtils.quit("Pass the juice!", this);
+					this.disconnect();
+					isAdminCommand = true;
+				}
+			}
+		}
+		if (!isAdminCommand) {
+			if (message[1].equals("PRIVMSG")) {
+				String line = BotUtils.joinStringFrom(message, 3);
+				if (line.matches("(?i)(.*)gas the(.*?)jews(.*)")) {
+					BotUtils.say("No, I said \"pass the juice.\"", message[2], this);
+				}
+			} else {
+				super.onMessage(message);
+			}
+		}
+	}
+	
+	/**
+	 * @param args the server and port to connect to
+	 */
+	public static void main(String[] args) {
+		String serverInfo = args[0];
+		String[] serverData = serverInfo.split(":");
+		JuiceBot juice = new JuiceBot(serverData[0], serverData[1]);
+		juice.getConfiguration().put("BOT_OWNER", "aaronweiss74");
+		BotBootstrap bootstrap = new BotBootstrap(juice);
+		bootstrap.run();
+		Scanner input = new Scanner(System.in);
+		while (true) {
+			String cmd = input.nextLine();
+			if (cmd.equalsIgnoreCase("quit") || cmd.equalsIgnoreCase("exit") || cmd.equalsIgnoreCase("shutdown")) {
+				bootstrap.shutdown();
+				break;
+			}
+		}
+		input.close();
+	}
+}
